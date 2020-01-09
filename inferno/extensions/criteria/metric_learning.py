fe783097057e6cd53d04f5e09cf9a29161f0657f
vzinche <valentyna.zinchenko@embl.de>
2020-01-09 15:36:20 +0100

    fixed and rewrote triplet loss

--- inferno/extensions/criteria/metric_learning.py
+++ inferno/extensions/criteria/metric_learning.py
@@ -1,26 +1,27 @@
-import numpy as np
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 __all__ = ['TripletLoss']
 
+
 class TripletLoss(nn.Module):
-    def __init__(self, margin=1.0, p=2, swap=False, absolute=False):
+    def __init__(self, margin=1.0, p=2, swap=False, absolute=False, reduction='mean'):
         super(TripletLoss, self).__init__()
-        self.margin = margin
-        self.p = p
-        self.swap = swap
         self.abs = absolute
+        if self.abs:
+            reduction = 'none'
+        if reduction == 'mean':     # pytorch, what's wrong with you
+            reduction = 'elementwise_mean'
+        self.loss = nn.TripletMarginLoss(margin=margin, p=p,
+                                         swap=swap, reduction=reduction)
+
     def forward(self, triplet, target=None):
-        #inferno trainer assumes every loss has a target; target not used here
+        # inferno trainer assumes every loss has a target; target not used here
         # we might or might not have the batch dim here
         assert len(triplet) == 3 or len(triplet[0]) == 3
         if len(triplet) == 3:
             triplet = triplet.unsqueeze(0)
-        losses = [F.triplet_margin_loss(batch[0], batch[1], batch[2],
-                                        margin=self.margin, swap=self.swap)
-                  for batch in triplet]
+        loss = self.loss(triplet[:, 0], triplet[:, 1], triplet[:, 2])
         if self.abs:
-            losses = np.array(losses, dtype='bool')
-        return sum(losses) / len(triplet)
+            loss = sum(loss > 0).type(torch.float) / len(triplet)
+        return loss
